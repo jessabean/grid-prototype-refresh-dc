08bf2392cc9d54b58add76146d283f573bebd801
Jessica Eldredge <jess@eldredge.io>
2017-09-10 10:24:23 -0400

    Initial commit - set up project

---
+++ README.md
@@ -0,0 +1,14 @@
+# Prototyping Layout with CSS Grid
+
+Source code for prototype presented at Refresh DC in September 2017.
+
+## Quick start
+
+```
+git clone git@github.com:jessabean/grid-prototype-refresh-dc.git
+cd grid-prototype-refresh-dc
+npm install
+node server.js
+```
+
+To make changes to the code and compile `.scss` files, run `gulp watch`.
---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var prefix      = require('gulp-autoprefixer');
+var sass        = require('gulp-sass');
+
+gulp.task('browser-sync', ['sass'], function() {
+    browserSync({
+        proxy: "http://localhost:4000"
+    });
+});
+
+gulp.task('browser-reload', function() {
+  browserSync.reload();
+});
+
+gulp.task('sass', function () {
+    return gulp.src('sass/main.scss')
+        .pipe(sass({
+            includePaths: ['sass'],
+            onError: browserSync.notify
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('public'));
+});
+
+
+gulp.task('watch', function () {
+    gulp.watch('sass/**/*.scss', ['sass']);
+    gulp.watch('**/*.html', ['browser-reload']);
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
